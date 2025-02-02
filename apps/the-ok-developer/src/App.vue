4b221cf4746c8f6c19d83f2fbc03331e1e817de8
Garren Riechel <garren@garrenriechel.com>
2025-02-02 11:14:40 -0700

    Fix build

--- apps/the-ok-developer/src/App.vue
+++ apps/the-ok-developer/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import Hello from '@repo/components/hello'
+import { Hello } from '@repo/components'
 </script>
 
 <template>
